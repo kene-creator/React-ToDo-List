1b6da981559dd93befa4b1233977df918664c47c
Kene <kayyungx@gmail.com>
2022-10-31 11:39:12 +0100

    Change li item to react component

---
+++ src/components/TodoItem.js
@@ -0,0 +1,3 @@
+import React from "react";
+const TodoItem = (props) => {};
+export default TodoItem;
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,10 +1,11 @@
 import React from "react";
+import TodoItem from "./TodoItem";
 
 const TodosList = (props) => {
   return (
     <ul>
       {props.todos.map((todo) => (
-        <li key={todo.id}>{todo.title}</li>
+        <TodoItem key={todo.id} todo={todo} />
       ))}
     </ul>
   );
